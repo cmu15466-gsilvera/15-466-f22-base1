d0619051e882e293d962214be654507a4d0759db
Gustavo Silvera <grs5382@gmail.com>
2022-06-30 16:39:07 -0700

    formatting with WebKit

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -1,35 +1,35 @@
-#include "PPU466.hpp"
 #include "Mode.hpp"
+#include "PPU466.hpp"
 
 #include <glm/glm.hpp>
 
-#include <vector>
 #include <deque>
+#include <vector>
 
 struct PlayMode : Mode {
-	PlayMode();
-	virtual ~PlayMode();
+    PlayMode();
+    virtual ~PlayMode();
 
-	//functions called by main loop:
-	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
-	virtual void update(float elapsed) override;
-	virtual void draw(glm::uvec2 const &drawable_size) override;
+    // functions called by main loop:
+    virtual bool handle_event(SDL_Event const&, glm::uvec2 const& window_size) override;
+    virtual void update(float elapsed) override;
+    virtual void draw(glm::uvec2 const& drawable_size) override;
 
-	//----- game state -----
+    //----- game state -----
 
-	//input tracking:
-	struct Button {
-		uint8_t downs = 0;
-		uint8_t pressed = 0;
-	} left, right, down, up;
+    // input tracking:
+    struct Button {
+        uint8_t downs = 0;
+        uint8_t pressed = 0;
+    } left, right, down, up;
 
-	//some weird background animation:
-	float background_fade = 0.0f;
+    // some weird background animation:
+    float background_fade = 0.0f;
 
-	//player position:
-	glm::vec2 player_at = glm::vec2(0.0f);
+    // player position:
+    glm::vec2 player_at = glm::vec2(0.0f);
 
-	//----- drawing handled by PPU466 -----
+    //----- drawing handled by PPU466 -----
 
-	PPU466 ppu;
+    PPU466 ppu;
 };
